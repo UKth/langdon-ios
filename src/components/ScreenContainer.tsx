e2c437fc00c3d2fe4ef2f38d21034d2a81132332
UKth <p.wilson0506@gmail.com>
2022-11-05 04:13:00 -0500

    :sparkles: add some components

---
+++ src/components/ScreenContainer.tsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { Keyboard } from "react-native";
+import { Pressable, View, ViewStyle } from "react-native";
+
+const ScreenContainer = ({
+  children,
+  style,
+}: {
+  children?: React.ReactNode;
+  style?: ViewStyle;
+}) => {
+  return (
+    <View
+      style={{
+        backgroundColor: "#ffffff",
+        flex: 1,
+        ...style,
+      }}
+    >
+      {children}
+    </View>
+  );
+};
+export default ScreenContainer;

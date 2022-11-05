a1cb46b57ecf424a8e75121d269559e4680e96c2
UKth <p.wilson0506@gmail.com>
2022-11-05 04:10:51 -0500

    :art: improve import structure

--- src/navigation/StackGenerator.tsx
+++ src/navigation/StackGenerator.tsx
@@ -1,14 +1,25 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Board from "../screens/MainTab/Board";
-import Boards from "../screens/MainTab/Boards";
-import TimeTable from "../screens/MainTab/TimeTable";
+import {
+  Board,
+  Boards,
+  Post,
+  TimeTable,
+  WritePost,
+} from "../screens/MainTab/index";
+
 import React, { useContext, useEffect } from "react";
+import { HeaderBackButton } from "../components/HeaderBackButton";
+import { colors } from "../constants/Colors";
+import EnrollClasses from "../screens/MainTab/EnrollClasses";
 
 export type StackGeneratorParamList = {
   TimeTable: undefined;
   Boards: undefined;
   Board: { id: number; title: string };
+  Post: { id: number };
+  WritePost: { boardId: number };
+  EnrollClasses: undefined;
 };
 
 export interface StackGeneratorProps {
@@ -25,14 +36,33 @@ const StackGenerator = ({ screenName }: StackGeneratorProps) => {
   // const { width } = useWindowDimensions();
 
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={({ route, navigation }) => {
+        return {
+          headerTintColor: colors.themeColor,
+          headerTitleStyle: {
+            fontFamily: "Arial Rounded MT Bold",
+          },
+          headerLeft: () =>
+            navigation.getState().routes.length > 1 ? (
+              <HeaderBackButton onPress={() => navigation.pop()} />
+            ) : null,
+        };
+      }}
+    >
       {screenName === "TimeTableStack" ? (
         <Stack.Screen name="TimeTable" component={TimeTable} />
       ) : null}
+      {screenName === "TimeTableStack" ? (
+        <Stack.Screen name="EnrollClasses" component={EnrollClasses} />
+      ) : null}
+
       {screenName === "BoardsStack" ? (
         <Stack.Screen name="Boards" component={Boards} />
       ) : null}
       <Stack.Screen name="Board" component={Board} />
+      <Stack.Screen name="Post" component={Post} />
+      <Stack.Screen name="WritePost" component={WritePost} />
     </Stack.Navigator>
   );
 };
---
+++ src/screens/MainTab/index.ts
@@ -0,0 +1,7 @@
+import Board from "./Board";
+import Boards from "./Boards";
+import Post from "./Post";
+import TimeTable from "./TimeTable";
+import WritePost from "./WritePost";
+
+export { Board, Boards, Post, TimeTable, WritePost };
